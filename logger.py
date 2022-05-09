5f8d29207190fe3e84626a527940739aa514f855
Kamil Wilczek <kamil.wilczek@igoriatrade.com>
2022-05-09 20:43:05 +0100

    logger file

---
+++ logger.py
@@ -0,0 +1,12 @@
+import datetime
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+log_dir = os.path.join(BASE_DIR, 'logs')
+os.makedirs(log_dir, exist_ok=True)
+
+def trigger_log_save(log_data=""):
+    filename = f'{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.txt'
+    filepath=os.path.join(log_dir, filename)
+    with open(filepath, 'w+') as f:
+        f.write(log_data)
